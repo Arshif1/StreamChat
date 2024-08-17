f87c3c6eaa8fda808bea2dc8e389a0910a13091e
Arshif1 <mohammedarshif456@gmail.com>
2024-08-17 12:25:17 +0400

    Initial Commit

---
+++ StreamChat/StreamChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamChatApp.swift
+//  StreamChat
+//
+//  Created by Arshif on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct StreamChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
