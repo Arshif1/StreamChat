ee099e2949296a2a6076a0edbc305545dc82e162
Arshif1 <mohammedarshif456@gmail.com>
2024-08-19 20:02:59 +0400

    create all channel controller

---
+++ Chat_UIKIt/AllChannelList.swift
@@ -0,0 +1,72 @@
+//
+//  AllChannelList.swift
+//  Chat_UIKIt
+//
+//  Created by Arshif on 19/08/2024.
+//
+
+import UIKit
+import StreamChat
+import StreamChatUI
+
+class AllChannelList: UIViewController {
+    
+    private var availableChannelTypes: [ChannelType]  = [.livestream, .messaging, .team, .gaming, .commerce]
+    
+    private lazy var segmentedControl: UISegmentedControl = {
+        let sc = UISegmentedControl(items: availableChannelTypes.map(\.rawValue.capitalized))
+        sc.selectedSegmentIndex = 0
+        let font = UIFont.systemFont(ofSize: 9)
+        sc.setTitleTextAttributes([NSAttributedString.Key.font: font], for: .normal)
+        sc.addTarget(self, action: #selector(segmentChanged(_:)), for: .valueChanged)
+        return sc
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+    }
+    
+    private func configure() {
+        navigationItem.titleView = segmentedControl
+        if let first = availableChannelTypes.first {
+            segmentChanged(for: first)
+        }
+    }
+    
+    @objc func segmentChanged(_ sender: UISegmentedControl) {
+        segmentChanged(for: availableChannelTypes[sender.selectedSegmentIndex])
+    }
+    
+    private func segmentChanged(for channelType: ChannelType) {
+        addChildVC(for: channelType)
+    }
+    
+    private func addChildVC(for channelType: ChannelType) {
+        let channelList = ChatChannelListVC()
+        let query = ChannelListQuery(filter: .equal("type", to: channelType.rawValue))
+        channelList.controller = ChatClient.shared.channelListController(query: query)
+        transitionToChildViewController(channelList)
+    }
+    
+    private func createChildVC(_ childVC: UIViewController) {
+        addChild(childVC)
+        view.addSubview(childVC.view)
+        childVC.view.frame = view.bounds
+        childVC.didMove(toParent: self)
+    }
+    
+    private func removeChildViewController(_ childVC: UIViewController) {
+        childVC.willMove(toParent: nil)
+        childVC.view.removeFromSuperview()
+        childVC.removeFromParent()
+    }
+    
+    private func transitionToChildViewController(_ childVC: UIViewController) {
+        if let currentChildVC = children.first {
+            removeChildViewController(currentChildVC)
+        }
+        createChildVC(childVC)
+    }
+}
--- Chat_UIKIt/UserChannelList.swift
+++ Chat_UIKIt/UserChannelList.swift
@@ -9,5 +9,33 @@ import StreamChat
 import StreamChatUI
 import UIKit
 
-class UserChannelList: ChatChannelListVC {}
+class UserChannelList: ChatChannelListVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
 
+        let buttonList = UIBarButtonItem(image: UIImage(systemName: "list.bullet.circle"),
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(tapActionList))
+        
+        let buttonAdd = UIBarButtonItem(image: UIImage(systemName: "plus"),
+                                        style: .plain,
+                                        target: self,
+                                        action: #selector(tapActionAdd))
+        
+        self.navigationItem.rightBarButtonItems = [buttonAdd, buttonList]
+    }
+    
+    @objc private func tapActionList() {
+        let conroller = UINavigationController(rootViewController: AllChannelList())
+        
+        present(conroller, animated: true, completion: nil)
+    }
+    
+    @objc private func tapActionAdd() {
+        print("Tapped Add Button")
+    }
+    
+}
